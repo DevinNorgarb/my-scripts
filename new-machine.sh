7e7957f912a30031403476f6cd0f6af57d72ebb7
Devin Norgarb <dnorgarb@gmail.com>
2021-05-28 23:20:02 +0200

    wip

---
+++ new-machine.sh
@@ -0,0 +1,30 @@
+
+sudo apt update
+
+
+
+
+ sudo apt install gdebi terminator docker docker-compose git wget curl python3 virt-viewer nmap  nginx
+ 
+ wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+ 
+ sudo gdebi google-chrome-stable_current_amd64.deb -y
+ 
+ 
+ ssh-keygen
+ 
+ sudo snap install slack --classic
+
+
+sudo apt update
+sudo apt install software-properties-common apt-transport-https wget
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update
+sudo apt install code
+
+wget https://release.axocdn.com/linux/GitKraken-v6.0.1.deb
+dpkg-deb -R GitKraken-v5.0.4.deb tmp
+
+	
+	
